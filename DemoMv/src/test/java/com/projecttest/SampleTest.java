7d20293fe16b7c5410860614acdc2e061b0e9de0
ADMIN <ADMIN@DESKTOP-Q43CBQE>
2021-03-15 12:57:21 +0530

    First Commit

---
+++ DemoMv/src/test/java/com/projecttest/SampleTest.java
@@ -0,0 +1,25 @@
+package com.projecttest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+		
+		@Test
+		public void createContact() {
+			System.out.println("exceute create contact");
+		}
+		
+		@Test
+		public void modifyContact() {
+			System.out.println("execute modify contact");
+		}
+		
+		@Test
+		public void deletecContact() {
+			System.out.println(" Delete contact");
+		}
+	
+	
+	
+}
+
